4f7fa8d58b47039783797670c35c99a4b8797a54
damdat97 <dam.dat97@gmail.com>
2022-03-07 17:23:57 +0700

    add

---
+++ js.js
@@ -0,0 +1,16 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('Enter width:');
+document.write('<br/>');
+document.write( + inputWidth);
+document.write('<br/>');
+document.write('Enter height:');
+document.write('<br/>');
+document.write( + inputHeight);
+document.write('<br/>');
+document.write("The area is:" +area);
